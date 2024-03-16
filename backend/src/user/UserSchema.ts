6156ced4c6185a7cd2371dbfb737b747d522a3c6
Joao Zacchello <xonguinhos@gmail.com>
2024-03-16 15:46:15 -0300

    add: user schema

---
+++ backend/src/user/UserSchema.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  sobrenome: { type: String, required: true },
+  email: { type: String, required: true },
+  senha: { type: String, required: true }
+})
+
+export const UserModel = mongoose.model("user", userSchema)
