9a1b387bec1c97491277f8563c107b85d34ff677
Joao Zacchello <xonguinhos@gmail.com>
2024-03-17 02:51:57 -0300

    better user controller

--- backend/src/user/UserController.ts
+++ backend/src/user/UserController.ts
@@ -6,32 +6,23 @@ const createUser = asyncHandler(async (req, res) => {
   const { auth, nome, sobrenome, email, senha } = req.body
 
   isAdmin(auth).then(async authorized => {
-    switch(authorized) {
-      case true:
-        const userAlreadyExists = await UserModel.findOne({ email: email })
-        if (userAlreadyExists) {
-          res.send("Email already exists").status(500)
-        } else {
-          UserModel.create({
-            nome: nome,
-            sobrenome: sobrenome,
-            email: email,
-            senha: senha
-          }).then(user => {
-            res.status(200).json(user)
-          }).catch(err => {
-            res.status(500).send(`Erro: ${err}`)
-          })
-        }
-        break
-      
-      case false:
-        res.send("Você não tem permissão para fazer isso!").status(500)
-        break
-
-      default:
-        break
-    }
+    if (authorized) {
+      const userAlreadyExists = await UserModel.findOne({ email: email })
+      if (userAlreadyExists) {
+        res.send("Email already exists").status(500)
+      } else {
+        UserModel.create({
+          nome: nome,
+          sobrenome: sobrenome,
+          email: email,
+          senha: senha
+        }).then(user => {
+          res.status(200).json(user)
+        }).catch(err => {
+          res.status(500).send(`Erro: ${err}`)
+        })
+      }
+    } else res.send("Você não tem permissão para fazer isso!").status(500)
   })
 })
 
@@ -39,23 +30,17 @@ const authorizeLogin = asyncHandler(async (req, res) => {
   const { auth, email, password } = req.body
 
   isAdmin(auth).then(async authorized => {
-    switch (authorized) {
-      case true:
-        const userExists = await UserModel.findOne({ email: email, senha: password })
-        if (userExists) {
-          res.send("Authorized").status(200)
-        } else {
-          res.send("Info doesn't match").status(500)
-        }
-        break
-
-      case false:
-        res.send("Você não tem permissão para fazer isso!").status(500)
-        break
-
-      default:
-        break
-    }
+    if (authorized) UserModel.findOne({ email: email, password: password }).then(exists => {
+      if (exists) {
+        res.send("Authorized").status(200)
+      } else {
+        res.send("Info doesn't match").status(500)
+      }
+    }).catch(err => {
+      console.log(`Error trying to authorize/login user!`)
+      res.status(500).send(`Error: ${err}`)
+    })
+    else res.send("Você não tem permissão para fazer isso!").status(500)
   })
 })
 
